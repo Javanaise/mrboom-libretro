e1e22534a93c5c30b8bdd1531c2ec7992a0bf1ce
twinaphex <libretro@gmail.com>
2017-04-14 20:06:55 +0200

    Don't need these files

--- ai/bt.hpp
+++ ai/bt.hpp
@@ -9,16 +9,9 @@
 #include "bt/Node.hpp"
 
 // composites
-#include "bt/composites/MemSelector.hpp"
-#include "bt/composites/MemSequence.hpp"
-#include "bt/composites/ParallelSequence.hpp"
 #include "bt/composites/Selector.hpp"
 #include "bt/composites/Sequence.hpp"
 
 // decorators
 #include "bt/decorators/Failer.hpp"
-#include "bt/decorators/Inverter.hpp"
-#include "bt/decorators/Repeater.hpp"
 #include "bt/decorators/Succeeder.hpp"
-#include "bt/decorators/UntilFail.hpp"
-#include "bt/decorators/UntilSuccess.hpp"
