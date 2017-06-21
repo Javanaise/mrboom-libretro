9287774684bb3d79dcab8e36c2acf563216bdcd1
franck <franck@mumblecore.org>
2017-06-21 21:40:08 +0200

    Bumped version to 3.3

--- common.hpp
+++ common.hpp
@@ -8,7 +8,7 @@
 extern "C" {
 #endif
 #define GAME_NAME          "Mr.Boom"
-#define GAME_VERSION       "3.2"
+#define GAME_VERSION       "3.3"
 #define PATH_MAX_LENGTH    256
 #define WIDTH              320
 #define HEIGHT             200
